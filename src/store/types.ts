1c1bfa53693363e12c3cd40b52ff89425276ec15
Sergey Klevakin <justerest@yandex.ru>
2018-07-09 11:04:14 +0500

    fix(store): change type of dispatch param to AnyAction

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,3 +1,4 @@
+import { AnyAction } from 'redux';
 import { IRootStore } from '.';
 
-export type AsyncAction = (dispatch: (action: any) => void, getStore: () => IRootStore) => Promise<void>;
+export type AsyncAction = (dispatch: (action: AnyAction) => void, getStore: () => IRootStore) => Promise<void>;
